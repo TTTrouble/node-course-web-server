424398a0b0915814b3c9489db8eb26677057954e
Vijay <mannevijay@gmail.com>
2019-02-17 15:14:49 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const hbs = require('hbs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'The Home Page',
+        welcomeMessage: 'This is my first website built from scratch!!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'The About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'This is NOT what should happen'
+    });
+});
+
+app.listen(8081, () => {
+    console.log('Server is up on port 8081');
+});
